917430b146330794a038892e889f7cdd5a51ccaa
NeerajaAttaluri <neerajaattaluri15@gmail.com>
2024-07-15 14:11:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+/* ----- TYPING EFFECT ----- */
+ var typingEffect = new Typed(".typedText",{
+    strings : ["Neeraja Attaluri."],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
